b7ecb3a2a31c52e8e96af466afa902383e3b5816
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2025-12-30 11:11:55 +0530

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 program
   .name("exon")
   .description("CLI to generate Express backend boilerplate")
-  .version("1.0.4");
+  .version("1.1.1");
 
 console.log(
   chalk.yellow(figlet.textSync("EXON", { horizontalLayout: "full" }))
@@ -60,6 +60,8 @@ program
         overwrite: true,
         filter: (src) => !["node_modules", "dist"].some((f) => src.includes(f)),
       });
+      await new Promise((r) => setTimeout(r, 10));
+
       console.log(
         chalk.green(`✅ ${answer.language} project created in ${targetDir}`)
       );
